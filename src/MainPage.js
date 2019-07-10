4e33e5b32d3334a0c959a1fb644b6444abdd402e
Firenza <chadoftedahl@gmail.com>
2019-07-10 13:48:02 +0300

    Added main page

---
+++ src/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function MainPage() {
+  return (
+    <>
+      <h3>Stuffz to do</h3>
+      <ul>
+        <li>
+          <Link to="/RepositoryTypes/">RepositoryTypes</Link>
+        </li>
+        <li> 
+          <Link to="/Dependencies/">Dependencies</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default MainPage;
