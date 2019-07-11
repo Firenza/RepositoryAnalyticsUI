6beb2de3aeac57c94e152d0ddd690c05aff8f398
Firenza <chadoftedahl@gmail.com>
2019-07-11 13:43:25 +0300

    Changed to function component

--- src/Dependencies.js
+++ src/Dependencies.js
@@ -1,47 +1,32 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import DependencyNameAutoComplete from './DependencyNameAutoComplete';
 import DependencyInfoList from './DependencyInfoList';
 
-class Dependencies extends React.Component {
-  constructor(props) {
-    super(props);
+function Dependencies(props){
+  const [selectedDependencyName, setSelectedDependencyName] = useState(null);
 
-    this.state = {
-      selectedDependencyName: null,
-      initialSelectedDependencyName: this.props.match && this.props.match.params && this.props.match.params.dependencyName
-    };
-
-    this.updateSelectedDependencyName = this.updateSelectedDependencyName.bind(this);
-  }
-
-  componentDidMount() {
-    // Update the list of an intial value was passed in.  
-    // Should probably just have the search component trigger this if it's given a default value
-    if (this.state.initialSelectedDependencyName) {
-      this.updateSelectedDependencyName(this.state.initialSelectedDependencyName);
+  useEffect(() => {
+    if(props.match.params.dependencyName){
+      updateSelectedDependencyName(props.match.params.dependencyName)
     }
-  }
-
-  updateSelectedDependencyName(name) {
-    this.props.history.push(`/dependencies/${name}`)
+  }, [] /* Only need to trigger this when the page is loaded to do a search based on a provided url param */);
   
-    this.setState({
-      selectedDependencyName: name
-    });
+  let updateSelectedDependencyName = (name) => {
+    props.history.push(`/dependencies/${name}`)
+  
+    setSelectedDependencyName(name);
   }
 
-  render() {
-    return (
-      <Grid container justify="center">
-        <Grid item xs={10} >
-          <DependencyNameAutoComplete onDependencySelected={this.updateSelectedDependencyName} initialValue={this.state.initialSelectedDependencyName} />
-          <DependencyInfoList dependencyName={this.state.selectedDependencyName} />
-        </Grid>
+  return(
+    <Grid container justify="center">
+      <Grid item xs={10} >
+        <DependencyNameAutoComplete onDependencySelected={updateSelectedDependencyName} initialValue={selectedDependencyName} />
+        <DependencyInfoList dependencyName={selectedDependencyName} />
       </Grid>
-    )
-  }
+    </Grid>
+  )
 }
 
 DependencyInfoList.propTypes = {
