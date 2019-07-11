a403427a63019c0e09b1155d0cae70cb2b06aaa6
Firenza <chadoftedahl@gmail.com>
2019-07-11 13:47:46 +0300

    Update to fuction

--- src/App.js
+++ src/App.js
@@ -3,33 +3,22 @@ import Dependencies from './Dependencies';
 import RepositoryTypes from './RepositoryTypes'
 import MainPage from './MainPage'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from "@material-ui/core/Typography";
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-   
-      <Router>
-         <AppBar position="static">
-             <Typography variant="h6" color="inherit">
-              Repository Analysis
-            </Typography>
+function App(){
+  return (
+    <Router>
+      <AppBar position="static">
+        <Typography variant="h6" color="inherit">
+          Repository Analysis
+        </Typography>
       </AppBar>
-        <Route path="/dependencies/:dependencyName?" component={Dependencies} />
-        <Route path="/repositorytypes/" component={RepositoryTypes} />
-        <Route path="/" exact component={MainPage} />
-      </Router>
-      </>
-    );
-  }
+      <Route path="/dependencies/:dependencyName?" component={Dependencies} />
+      <Route path="/repositorytypes/" component={RepositoryTypes} />
+      <Route path="/" exact component={MainPage} />
+    </Router>
+  )
 }
 
 export default App;
