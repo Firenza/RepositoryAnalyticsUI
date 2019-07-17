e306649d6c8e4a66d6de46459266cbd09deae45e
Firenza <chadoftedahl@gmail.com>
2019-07-17 20:45:07 +0200

    Added JssProvider component to alleviate prod build issues

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import Dependencies from './Dependencies';
-import RepositoryTypes from './RepositoryTypes'
-import MainPage from './MainPage'
+import React from "react";
+import Dependencies from "./Dependencies";
+import RepositoryTypes from "./RepositoryTypes";
+import MainPage from "./MainPage";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import AppBar from '@material-ui/core/AppBar';
+import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
+import JssProvider from "react-jss/lib/JssProvider";
 
-function App(){
+function App() {
   return (
-    <Router>
-      <AppBar position="static">
-        <Typography variant="h6" color="inherit">
-          Repository Analysis
-        </Typography>
-      </AppBar>
-      <Route path="/dependencies/:dependencyName?" component={Dependencies} />
-      <Route path="/repositorytypes/" component={RepositoryTypes} />
-      <Route path="/" exact component={MainPage} />
-    </Router>
-  )
+    // Using this component to get around the CSS styling issues when doing a production build
+    <JssProvider>
+      <Router>
+        <AppBar position="static">
+          <Typography variant="h6" color="inherit">
+            Repository Analysis
+          </Typography>
+        </AppBar>
+        <Route path="/dependencies/:dependencyName?" component={Dependencies} />
+        <Route path="/repositorytypes/" component={RepositoryTypes} />
+        <Route path="/" exact component={MainPage} />
+      </Router>
+    </JssProvider>
+  );
 }
 
 export default App;
